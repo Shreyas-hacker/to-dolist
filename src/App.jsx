21ada8fb61cf9bf9639eaf139e305c398f2559f1
Shreyas-hacker <shreyashegde1@gmail.com>
2021-06-29 21:19:03 +0800

    given onchange, onclick functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 
 function App(){
+    const [inputText, setText] = React.useState("");
+    const [items, setItems] = React.useState([]);
+
+    function handleChange(event){
+        const inputValue = event.target.value;
+        setText(inputValue);
+    }
+    function handleClick(){
+        setItems((prevValue)=>{
+            return [...prevValue,inputText];
+        });
+        setText("");
+    }
     return (
     <div className="container">
         <div className="heading">
             <h1>To-Do List</h1>
         </div>
         <div className="form">
-            <input type="text"/>
-            <button>
+            <input onChange={handleChange} type="text" value={inputText}/>
+            <button onClick={handleClick}>
                 <span>Add</span>
             </button>
         </div>
         <ul>
-            <li>Added Item</li>
+            {items.map((item,index)=>{
+                return <li key={index}>{item}</li>
+            })}
         </ul>
     </div>)
 }
