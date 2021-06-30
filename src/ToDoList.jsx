769db0fcdb906d00e01128104ad8de8080bd9a89
Shreyas-hacker <shreyashegde1@gmail.com>
2021-06-30 18:16:59 +0800

    error: doneitems to doneitem

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function List(props){
-    return <li onClick={()=>{props.doneItems(props.id)}}>{props.listItems}</li>
+    return <li onClick={()=>{props.doneItem(props.id)}}>{props.listItems}</li>
 }
 
 export default List;
